c6ba46bf9856961a997cf34257df7365011ab66d
Ugljesa Kuzmanov <77068701+Veliki5382@users.noreply.github.com>
2024-07-18 03:08:20 +0200

    skoro spremno za treniranje

--- dataset.py
+++ dataset.py
@@ -4,15 +4,13 @@ from PIL import Image
 import pandas as pd
 from torchvision import transforms
 
-df = pd.read_csv('english.csv')
-df.head()
-
 class Data(Dataset):
-    def __init__(self, path_to_file):
+    def __init__(self, path_to_file, image_dim):
         self.data = pd.read_csv(path_to_file)
         self.transform = transforms.Compose([
             transforms.ToTensor(),
-            transforms.Normalize((0.5,), (0.5,))
+            transforms.Normalize((0.5,), (0.5,)),
+            transforms.Resize((image_dim, image_dim))
         ])
 
         self.images = self.data.iloc[:, 0]
@@ -26,8 +24,7 @@ class Data(Dataset):
         label = self.captions[idx]
 
         image = Image.open(image_path).convert('L')
-        if self.transform is not None:
-            image = self.transform(image)
+        image = self.transform(image)
 
         # pretvoriti u torch tensor
         return image, label
--- train.py
+++ train.py
@@ -1,3 +1,5 @@
+from calendar import c
+from locale import currency
 from PIL import Image
 import os
 from network import *
@@ -6,25 +8,27 @@ from dataset import *
 device = 'cpu'
 learn_rate = 3e-4
 z_dim = 32
-img_dim = 28*28
+img_dim = 28
 batch_size = 32
-num_epochs = 10
+num_epochs = 1
 
-train_dataset = Data('english.csv')
+train_dataset = Data('english.csv', img_dim)
 train_dataloader = DataLoader(train_dataset, batch_size, shuffle = True)
-gan = GAN(img_dim, z_dim).to(device)
+gan = GAN(img_dim*img_dim, z_dim).to(device)
 
 
 for epoch in range(num_epochs):
     batch_idx = 0
+
     for image, label in train_dataloader:
-        print(image.shape, label)
-       
+        curr_batch_size = len(image)
         ### Train Discriminator: max log(D(x)) + log(1 - D(G(z)))
         gan.disc_opt.zero_grad()
+        
         fake = gan.gen(torch.randn(batch_size, z_dim).to(device))
-        disc_real = gan.disc(image.view(1, -1))
-        disc_fake = gan.disc(fake.view(1, -1).detach())
+        print(image.shape)
+        disc_real = gan.disc(image.view(curr_batch_size, -1))
+        disc_fake = gan.disc(fake.view(curr_batch_size, -1).detach())
         
         lossD_real = gan.criterion(disc_real, torch.ones_like(disc_real))
         lossD_fake = gan.criterion(disc_fake, torch.zeros_like(disc_fake))
@@ -35,7 +39,7 @@ for epoch in range(num_epochs):
 
         ### Train Generator: min log(1 - D(G(z))) <-> max log(D(G(z))
         gan.gen_opt.zero_grad()
-        output = gan.disc(fake.view(1, -1))
+        output = gan.disc(fake.view(curr_batch_size, -1))
         
         lossG = gan.criterion(output, torch.ones_like(output))
         lossG.backward()
@@ -46,7 +50,7 @@ for epoch in range(num_epochs):
         batch_idx += 1
         if batch_idx % 100 == 0:
             print(
-                f"Epoch [{epoch}/{num_epochs}] Batch {batch_idx}/{len(train_dataset)} \
+                f"Epoch [{epoch+1}/{num_epochs}] Batch {batch_idx}/{len(train_dataset)} \
                   Loss D: {lossD:.4f}, loss G: {lossG:.4f}"
             )
 
